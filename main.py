0605f0ec1016b8888775cccedfcac5390f89558f
joelr4mire5 <joel.r4mire5@outlook.com>
2024-02-22 16:44:28 -0600

    la funcion de resta fue agregada

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def suma(num1:int,num2:int):
+def suma(num1:int,num2:int)->int:
     return num1+num2
 
-def resta(num1:int,num2:int):
+
+def resta(num1:int,num2:int)->int:
     return num1+num2
+
 
 if __name__ == '__main__':
     print(suma(2,4))
+    print(resta(2,5))
+
