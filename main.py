c27e11ff74ae7da434e7551c771de539b99a67c3
BrenMub <bmurillob11@gmail.com>
2024-02-22 18:21:15 -0600

    Gato function added

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ def suma(num1:int,num2:int)->int:
 def resta(num1:int,num2:int)->int:
     return num1+num2
 
+def gato(animal: str):
+    if animal == 'cat':
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     print(suma(2,4))
     print(resta(2,5))
+    print(gato(animal = 'cat'))
+
 
